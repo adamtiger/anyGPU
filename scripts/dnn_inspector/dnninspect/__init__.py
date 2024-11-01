11bf6040d6c2a4f4e3c84e3a5fb1c6602d09c262
Adam Budai <budai8adam@gmail.com>
2024-11-01 18:32:01 +0100

    inspector fix

--- scripts/dnn_inspector/dnninspect/__init__.py
+++ scripts/dnn_inspector/dnninspect/__init__.py
@@ -4,4 +4,5 @@ from dnninspect.nn_module import set_inspection_output_folder
 from dnninspect.nn_module import inspect_torch_module
 from dnninspect.nn_module import inspect_torch_tensor
 from dnninspect.nn_module import inspect_function
+from dnninspect.nn_module import inspect_function_repeated
 from dnninspect.nn_module import save_function_calc
