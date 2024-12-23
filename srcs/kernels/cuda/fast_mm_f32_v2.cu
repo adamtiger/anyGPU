0d27bb31b1a3c54f0a8a678b4bb71d0c413fea34
Adam Budai <budai8adam@gmail.com>
2024-12-23 17:29:41 +0100

    fp32 v2 for fast mm

--- srcs/kernels/cuda/fast_mm_f32_v2.cu
+++ srcs/kernels/cuda/fast_mm_f32_v2.cu
@@ -1,14 +1,13 @@
 #include "fast_mm.cuh"
 
-constexpr int NUM_WARPS = 8;
+constexpr int NUM_WARPS = 16;
 constexpr int WARP_SIZE = 32;
 
-constexpr int TS_H = 32;
+constexpr int TS_H = 64;
 constexpr int TS_K = 64;
-constexpr int TS_W = 32;
+constexpr int TS_W = 64;
 
-constexpr int NR = TS_H / NUM_WARPS;        // number of xt rows, used by a warp
-constexpr int NR_LOAD = TS_K / NUM_WARPS;   // during glob -> shared load each warp handles more rows
+constexpr int NR = TS_H / NUM_WARPS * (TS_W / WARP_SIZE);        // number of xt rows, used by a warp
 
 __global__ void cu_fast_mm_f32_v2_kernel(
 	const int x_width,
@@ -20,10 +19,9 @@ __global__ void cu_fast_mm_f32_v2_kernel(
 	
 	int NUM_KTILES = x_width / TS_K;  // number tiles along the common dimension
 	
-	int j = blockIdx.x * blockDim.x + threadIdx.x;
-	int i0 = blockIdx.y * TS_H;
-	int imax = TS_H;
 	int lane_id = threadIdx.y;
+	int j = blockIdx.x * TS_W + threadIdx.x + WARP_SIZE * (lane_id % 2);
+	int i0 = blockIdx.y * TS_H;
 
 	// outputs can be stored in local memory for speed
 	float32 y[NR] = {};
@@ -37,28 +35,23 @@ __global__ void cu_fast_mm_f32_v2_kernel(
 	{
 		// load the data from glob xt to shared mem
 		__shared__ float32 shrd_x[TS_H * TS_K];
-		for (int r = 0; r < NR_LOAD; ++r)
+		for (int r = 0; r < NR; ++r)
 		{
 			int wrow_id = lane_id / 2;
-			int shrd_index_x = (wrow_id * NR_LOAD + r) * TS_K + WARP_SIZE * (lane_id % 2) + threadIdx.x;
+			int shrd_index_x = (wrow_id * NR + r) * TS_K + WARP_SIZE * (lane_id % 2) + threadIdx.x;
 
-			if (wrow_id * NR_LOAD + r < imax)
-			{
-				int glob_index_x = (i0 + wrow_id * NR_LOAD + r) * x_width + tk * TS_K + WARP_SIZE * (lane_id % 2) + threadIdx.x;
-				shrd_x[shrd_index_x] = dx[glob_index_x];
-			}
-			else
-			{
-				shrd_x[shrd_index_x] = 0.f;
-			}
+			int glob_index_x = (i0 + wrow_id * NR + r) * x_width + tk * TS_K + WARP_SIZE * (lane_id % 2) + threadIdx.x;
+			shrd_x[shrd_index_x] = dx[glob_index_x];
 		}
 
 		// load the data from glob w to shared mem
 		__shared__ float32 shrd_w[TS_K * TS_W];
-		for (int r = 0; r < NR_LOAD; ++r)
+		for (int r = 0; r < NR; ++r)
 		{
-			int glob_index_w = (tk * TS_K + lane_id * NR_LOAD + r) * w_width + j;
-			int shrd_index_w = (lane_id * NR_LOAD + r) * TS_W + threadIdx.x;
+			int wrow_id = lane_id / 2;
+			int shrd_index_w = (wrow_id * NR + r) * TS_W + WARP_SIZE * (lane_id % 2) + threadIdx.x;
+
+			int glob_index_w = (tk * TS_K + wrow_id * NR + r) * w_width + j;
 			shrd_w[shrd_index_w] = dw[glob_index_w];
 		}
 
@@ -69,8 +62,10 @@ __global__ void cu_fast_mm_f32_v2_kernel(
 		{
 			for (int k = 0; k < TS_K; ++k)
 			{
-				int index_x = (lane_id * NR + r) * TS_K + k;
-				int index_w = k * TS_W + threadIdx.x;
+				int wrow_id = lane_id / 2;
+
+				int index_x = (wrow_id * NR + r) * TS_K + k;
+				int index_w = k * TS_W + WARP_SIZE * (lane_id % 2) + threadIdx.x;
 				y[r] += shrd_x[index_x] * shrd_w[index_w];
 			}
 		}
@@ -81,7 +76,9 @@ __global__ void cu_fast_mm_f32_v2_kernel(
 	// store results back in the global memory
 	for (int r = 0; r < NR; ++r)
 	{
-		int i = i0 + lane_id * NR + r;
+		int wrow_id = lane_id / 2;
+
+		int i = i0 + wrow_id * NR + r;
 		int glob_index_y = i * w_width + j;
 		dy[glob_index_y] = y[r];
 	}
